60d0623e963cebc30e9c0c0a4b1a325018e2de97
mbarrena <70719416+mbarrena@users.noreply.github.com>
2024-10-11 17:10:33 -0300

    Código para instalación de librerías en entorno

---
+++ scripts/install_libraries.sh
@@ -0,0 +1,8 @@
+pip install matplotlib
+pip install seaborn
+pip install --upgrade 'xlrd>=1.2.0'
+pip install scikit-learn
+#pip install chowtest   #Si falla esta lista ir a Entorno de ejecucion > reiniciar entorno de ejecucion y volver a correr esta celda.
+pip3 uninstall statsmodels -y
+pip install statsmodels --upgrade
+pip install arch
